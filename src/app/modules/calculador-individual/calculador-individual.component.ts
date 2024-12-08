000edc4641cbaa1327dbe6fc034d489ec5cc25c6
Luis <U20101321@utp.edu.pe>
2024-12-07 19:35:29 -0500

    gestion de calculador

--- src/app/modules/calculador-individual/calculador-individual.component.ts
+++ src/app/modules/calculador-individual/calculador-individual.component.ts
@@ -12,11 +12,19 @@ import Swal from 'sweetalert2';
 })
 export class CalculadorIndividualComponent implements OnInit {
 
+  btnPaso: string = 'Editar';
   currentYear!: number;
   nombresApellidos?: string;
   totalAnual: number = 0
   displayedColumns: string[] = ['periodo', 'conceptosFijos', 'bonificaciones', 'otrasEntidades', 'reintegros', 'totalMensual'];
+  displayedColumns1: string[] = ['periodo', 'deduccion', 'importe'];
+  displayedColumns2: string[] = ['rangos', 'importe', 'tasa', 'irqc'];
+  displayedColumns3: string[] = ['periodo', 'descuento', 'situacion'];
   dataSource: any[] = [];
+  dataSource1: any[] = [];
+  dataSource2: any[] = [];
+  dataSource3: any[] = [];
+  paso: number = 1;
   
   // Formulario reactivo
   codigoModularForm: FormGroup;
@@ -34,7 +42,38 @@ export class CalculadorIndividualComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dataSource1 = [
+      { periodo: '2025', deduccion: '36,050.00', importe: '65,000.00' }
+    ];
+
+    this.dataSource2 = [
+      { rangos: 'Hasta 5 UIT', importe: 'S/. -51.00', tasa: '8%', irqc: '4.08' },
+      { rangos: 'Más de 5 UIT hasta 20 UIT', importe: 'S/ .00', tasa: '14%', irqc: '-' },
+      { rangos: 'Más de 20 UIT hasta 35 UIT', importe: 'S/. 00', tasa: '17%', irqc: '-' },
+      { rangos: 'Más de 35 UIT hasta 45 UIT', importe: 'S/. 00', tasa: '20%', irqc: '-' },
+      { rangos: 'Más de 45 UIT', importe: 'S/. 00', tasa: '30%', irqc: '-' },
+      { rangos: 'TOTAL ANUAL DE IMPUESTO DE RENTA DE QUINTA CATEGORÍA (IRQC)', importe: '', tasa: '', irqc: '4.08' }
+    ];
+    
+
+    this.dataSource3 = [
+      { periodo: 'Ene-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Feb-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Mar-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Abr-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'May-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Jun-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Jul-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Ago-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Sep-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Oct-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Nov-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'Dic-24', descuento: '', situacion: 'Ejecutado' },
+      { periodo: 'TOTAL', descuento: '', situacion: 'Monto Recalculado' }
+    ];
+    
+  }
 
   // Método que se ejecuta cuando se hace clic en el botón de buscar
   onSearch(): void {
@@ -70,22 +109,18 @@ export class CalculadorIndividualComponent implements OnInit {
 
   // Función para activar o desactivar el modo de edición
   toggleEditMode(): void {
-    this.editMode = !this.editMode;
-    this.totalAnual = 0;
-    for(let item of this.dataSource){
-      item.totalPagar = parseInt(item.bonificaciones) + parseInt(item.conceptosFijos) + parseInt(item.reintegros) + (parseInt(item.otrasEntidades) || 0);
-      this.totalAnual += item.totalPagar;
+    if(this.btnPaso === 'Editar'){
+      this.editMode = !this.editMode;
+      this.totalAnual = 0;
+      for(let item of this.dataSource){
+        item.totalPagar = parseInt(item.bonificaciones) + parseInt(item.conceptosFijos) + parseInt(item.reintegros) + (parseInt(item.otrasEntidades) || 0);
+        this.totalAnual += item.totalPagar;
+      }
+    }else if(this.btnPaso === 'Regresar'){
+      this.btnPaso = 'Editar';
+        this.paso = 1;
     }
-  }
 
-  // Función para mostrar alertas de éxito
-  private showSuccessAlert(message: string): void {
-    Swal.fire({
-      title: 'Éxito',
-      text: message,
-      icon: 'success',
-      confirmButtonText: 'Aceptar'
-    });
   }
 
   // Función para mostrar alertas de error
@@ -97,4 +132,10 @@ export class CalculadorIndividualComponent implements OnInit {
       confirmButtonText: 'Aceptar'
     });
   }
+  
+  next(){
+    this.paso = 2;
+    this.btnPaso = 'Regresar';
+  }
+
 }
